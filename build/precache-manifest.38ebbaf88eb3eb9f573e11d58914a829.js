dd0112e55057197314ff111959e09488ebe3e50b
dexkode4 <dexkode4@gmail.com>
2020-10-16 19:17:29 +0100

    first build

---
+++ build/precache-manifest.38ebbaf88eb3eb9f573e11d58914a829.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6e51a73611942ff22641b4446055a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d385e579bea34dc2e92a",
+    "url": "/static/js/2.c33d4828.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c33d4828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ecd4b9e23a71b823fa1",
+    "url": "/static/js/main.329b0812.chunk.js"
+  },
+  {
+    "revision": "0cead240beaeac72778b",
+    "url": "/static/js/runtime-main.50b5716e.js"
+  },
+  {
+    "revision": "3566bc377d5b0fdc2ea668a93766c67f",
+    "url": "/static/media/pure-julia.3566bc37.jpg"
+  }
+]);
