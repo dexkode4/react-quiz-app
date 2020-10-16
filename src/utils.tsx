df995e672d14cd386a06a86d1fc6c92a494a01f4
dexkode4 <dexkode4@gmail.com>
2020-10-16 14:44:37 +0100

    created shuffle Array util method  and fetchQuizQuestion function

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,11 +1,1 @@
-import React from 'react'
-
-const utils = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default utils
+export const shuffleArray = (array:any[]) => [...array].sort(() => Math.random() - 0.5);
